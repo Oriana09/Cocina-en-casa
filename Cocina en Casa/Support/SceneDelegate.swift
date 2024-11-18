4a107143f85224437bd9371df9214c093e639971
oriana09 <naircostancio@gmail.com>
2024-11-17 23:30:41 -0300

    Primer commit

---
+++ Cocina en Casa/Support/SceneDelegate.swift
@@ -0,0 +1,53 @@
+//
+//  SceneDelegate.swift
+//  Cocina en Casa
+//
+//  Created by Oriana Costancio on 17/11/2024.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    
+    var window: UIWindow?
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        self.setNavigationBarAppearance()
+        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+        window?.windowScene = windowScene
+        window?.rootViewController = HomeViewController()
+        window?.makeKeyAndVisible()
+        
+    }
+
+    func setNavigationBarAppearance() {
+        if #available(iOS 15, *) {
+            let navBarAppearance: UINavigationBarAppearance = UINavigationBarAppearance()
+            navBarAppearance.configureWithOpaqueBackground()
+            navBarAppearance.shadowColor = .clear
+            UINavigationBar.appearance().tintColor = .systemGreen
+            UINavigationBar.appearance().standardAppearance = navBarAppearance
+            UINavigationBar.appearance().scrollEdgeAppearance = navBarAppearance
+        } else {
+            UINavigationBar.appearance().tintColor = .systemGreen
+        }
+    }
+    
+    
+    func sceneDidDisconnect(_ scene: UIScene) {
+    }
+    func sceneDidBecomeActive(_ scene: UIScene) {
+    }
+    func sceneWillResignActive(_ scene: UIScene) {
+    }
+    func sceneWillEnterForeground(_ scene: UIScene) {
+    }
+    func sceneDidEnterBackground(_ scene: UIScene) {
+        
+    }
+}
+
+
+
